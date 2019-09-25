838c7e8677cd9e0aec080ea9624dd43d6c2848d9
QuyNguyen <quy.n.nguyen@sjsu.edu>
2019-09-25 05:42:56 -0700

    Create Maven Project ( added Eclipse files )

---
+++ src/test/java/Selenium-Build-Framework/AppTest.java
@@ -0,0 +1,20 @@
+package Selenium-Build-Framework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
