8574943195b36ce731f3a3037dde68d1f893f3d3
Quy Nguyen <52801407+mlucario88@users.noreply.github.com>
2019-09-25 08:22:42 -0700

    Please Merge to the master branch (#1)
    * Modified Project
    
    * Setup base class and some test

---
+++ src/test/java/Mlucario/HomePage.java
@@ -0,0 +1,14 @@
+package Mlucario;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class HomePage extends base {
+
+	@Test
+	public void basePageNavigation() throws IOException {
+		driver = initializeDriver();
+		driver.get("http:\\google.com");
+	}
+}
