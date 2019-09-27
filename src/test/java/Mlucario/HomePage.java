fa95123063ce74f4cd0044a78d3315e10893c89f
QuyNguyen <quy.n.nguyen@sjsu.edu>
2019-09-27 08:22:30 -0700

    readme

--- src/test/java/Mlucario/HomePage.java
+++ src/test/java/Mlucario/HomePage.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
+import pageObject.LandingPage;
+
 public class HomePage extends base {
 
 	@Test
 	public void basePageNavigation() throws IOException {
 		driver = initializeDriver();
-		driver.get("http:\\google.com");
+		driver.get("http://www.qaclickacademy.com/");
+
+		LandingPage ld = new LandingPage(driver);
+		ld.getLogin().click();
+
 	}
 }
